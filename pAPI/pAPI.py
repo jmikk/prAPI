15c1bd9641b79dfd695a37a60c1c7e4beed09e62
jmikk <buldog.buldog26@gmail.com>
2023-05-17 11:27:32 -0500

    Update pAPI.py

--- pAPI/pAPI.py
+++ pAPI/pAPI.py
@@ -14,7 +14,7 @@ class pAPI(commands.Cog):
     async def pAPI_version(self,ctx):
         await ctx.send("This is version 1.2")
         
-    def api_request(self,data, header,_limit=0):
+    async def api_request(self,data, header,_limit=0):
         if _limit > 50:
             await ctx.send("Sorry no API limit left for you to use I tried 50 times!")
             return
