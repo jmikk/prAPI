d384744c0b383581c819653bb99296ecb3c82a14
jmikk <buldog.buldog26@gmail.com>
2023-06-03 16:35:37 -0500

    Update GiveAway.py

--- GiveAway/GiveAway.py
+++ GiveAway/GiveAway.py
@@ -22,7 +22,7 @@ class GiveAway(commands.Cog):
             return f"{seconds} seconds"
 
     def format_timestamp(self, timestamp):
-        return f"<t:{timestamp/1000}:R>"
+        return f"<t:{timestamp/100}:R>"
 
     @commands.command()
     @commands.has_permissions(administrator=True)
