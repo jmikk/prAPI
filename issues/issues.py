183c48a5b50a0887d79e2eac08e5b8878dc5672e
jmikk <buldog.buldog26@gmail.com>
2023-06-02 22:13:47 -0500

    Update issues.py

--- issues/issues.py
+++ issues/issues.py
@@ -59,7 +59,7 @@ class issues(commands.Cog):
         await ctx.send("idk boss")
         r = await self.api_request(data={'nation': self.IssueNation, 'q': 'issues'})
         # Extracting data from the parsed XML
-        issues.findall("issue")
+        r.xml.findall("issue")
         for issue in issues:
             title = issue.find('title').text
             text = issue.find('text').text
