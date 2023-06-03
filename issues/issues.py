62aad0892083e457c4d016611d566360b83d6e22
jmikk <buldog.buldog26@gmail.com>
2023-06-02 22:18:20 -0500

    Update issues.py

--- issues/issues.py
+++ issues/issues.py
@@ -61,6 +61,7 @@ class issues(commands.Cog):
         # Extracting data from the parsed XML
         issues=r.xml.findall("issue")
         await ctx.send(issues)
+        await ctx.send(r.text)
         for issue in issues:
             title = issue.find('title').text
             text = issue.find('text').text
