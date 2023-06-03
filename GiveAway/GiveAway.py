67ac44a0fb2e634e225d95de73bbbc0376804498
jmikk <buldog.buldog26@gmail.com>
2023-06-03 15:18:57 -0500

    Update GiveAway.py

--- GiveAway/GiveAway.py
+++ GiveAway/GiveAway.py
@@ -35,7 +35,9 @@ class GiveAway(commands.Cog):
 
         new_message = await channel.fetch_message(message.id)
         reaction = discord.utils.get(new_message.reactions, emoji="🎉")
-        participants = await list(reaction.users())
+        participants = []
+        async for participant in reaction.users():
+            participants.append(participant)
         eligible_participants = [
             participant
             for participant in participants
