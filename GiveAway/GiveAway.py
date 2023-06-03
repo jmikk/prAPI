912e377474eed93478dc3e97f12c82d513aad8eb
jmikk <buldog.buldog26@gmail.com>
2023-06-03 16:51:49 -0500

    Update GiveAway.py

--- GiveAway/GiveAway.py
+++ GiveAway/GiveAway.py
@@ -7,39 +7,38 @@ from datetime import datetime, timedelta
 class GiveAway(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.giveaway_channel_id = None
-        self.current_giveaway = None
+        self.giveaways = {}
 
-    def format_duration(self, duration):
-        hours, remainder = divmod(duration, 3600)
-        minutes, seconds = divmod(remainder, 60)
+    def generate_giveaway_id(self):
+        while True:
+            giveaway_id = random.randint(1000, 9999)
+            if giveaway_id not in self.giveaways:
+                return giveaway_id
 
-        if hours > 0:
-            return f"{hours} hours, {minutes} minutes"
-        elif minutes > 0:
-            return f"{minutes} minutes, {seconds} seconds"
-        else:
-            return f"{seconds} seconds"
-
-    def format_timestamp(self, timestamp):
-        return f"<t:{timestamp-18000}:R>"
+    # ... Existing code ...
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def startgiveaway(self, ctx, duration: int, prize: str, *roles: discord.Role):
-        if self.current_giveaway is not None:
-            await ctx.send("A giveaway is already running.")
+        giveaway_id = self.generate_giveaway_id()
+
+        if giveaway_id in self.giveaways:
+            await ctx.send("A giveaway is already running with the same ID. Please try again.")
             return
 
-        self.current_giveaway = {
-            "end_time": datetime.utcnow().timestamp() + timedelta(seconds=duration).total_seconds(),
+        end_time = datetime.utcnow() + timedelta(seconds=duration)
+        formatted_duration = self.format_duration(duration)
+
+        giveaway_data = {
+            "end_time": end_time,
             "prize": prize,
             "roles": roles,
             "participants": []
         }
 
-        formatted_duration = self.format_duration(duration)
-        end_timestamp = int((datetime.utcnow() + timedelta(seconds=duration)).timestamp())
+        self.giveaways[giveaway_id] = giveaway_data
+
+        end_timestamp = int(end_time.timestamp())
         message = (
             f"🎉 **Giveaway** 🎉\n\n"
             f"React with 🎉 to enter the giveaway!\n"
@@ -47,12 +46,12 @@ class GiveAway(commands.Cog):
             f"Ends in {self.format_timestamp(end_timestamp)}."
         )
 
-        channel = self.bot.get_channel(self.giveaway_channel_id)
+        channel = ctx.channel  # Use the current channel for the giveaway message
         sent_message = await channel.send(message)
         await sent_message.add_reaction("🎉")
 
         await asyncio.sleep(duration)
-        self.current_giveaway = None
+        del self.giveaways[giveaway_id]
 
         new_message = await channel.fetch_message(sent_message.id)
         reaction = discord.utils.get(new_message.reactions, emoji="🎉")
@@ -64,28 +63,23 @@ class GiveAway(commands.Cog):
 
         if participants:
             winner = random.choice(participants)
-            await channel.send(f"Congratulations to {winner.mention} for winning the giveaway!")
+            await channel.send(f"Congratulations to {winner.mention} for winning the giveaway ({giveaway_id})!")
         else:
-            await channel.send("No eligible participants. The giveaway has ended.")
+            await channel.send(f"No eligible participants. The giveaway ({giveaway_id}) has ended.")
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if self.current_giveaway is not None:
+        for giveaway_id, giveaway_data in self.giveaways.items():
             channel_id = payload.channel_id
             message_id = payload.message_id
             user_id = payload.user_id
 
-            if channel_id == self.giveaway_channel_id and message_id == self.current_giveaway["message_id"]:
+            if (
+                channel_id == payload.channel_id
+                and message_id == giveaway_data["message_id"]
+            ):
                 guild = self.bot.get_guild(payload.guild_id)
                 member = guild.get_member(user_id)
 
-                if any(role in member.roles for role in self.current_giveaway["roles"]):
-                    self.current_giveaway["participants"].append(member)
-                    
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def setgiveawaychannel(self, ctx, channel: discord.TextChannel):
-        self.giveaway_channel_id = channel.id
-        await ctx.send(f"Giveaway channel set to {channel.mention}")
-                
-    
+                if any(role in member.roles for role in giveaway_data["roles"]):
+                    giveaway_data["participants"].append(member)
