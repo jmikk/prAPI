9dfe2137550567d12f21e7d127915df6a16bdeed
jmikk <buldog.buldog26@gmail.com>
2023-06-03 12:14:50 -0500

    Update GiveAway.py

--- GiveAway/GiveAway.py
+++ GiveAway/GiveAway.py
@@ -1,35 +1,62 @@
-from redbot.core import commands
+import discord
+from discord.ext import commands
 import asyncio
 
-
-def is_owner_overridable():
-    # Similar to @commands.is_owner()
-    # Unlike that, however, this check can be overridden with core Permissions
-    def predicate(ctx):
-        return False
-
-    return commands.permissions_check(predicate)
-
-
-class GiveAway(commands.Cog):
-    """My custom cog"""
-
+class Giveaway(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    def cog_unload(self):
-        asyncio.create_task(self.client.aclose())
-
-    # great once your done messing with the bot.
-    #   async def cog_command_error(self, ctx, error):
-    #       await ctx.send(" ".join(error.args))
+        self.giveaway_channel_id = 1114598850049618060
+        self.current_giveaway = None
 
     @commands.command()
-    @commands.is_owner()
-    async def myCom1(self, ctx):
-        await ctx.send("I work")
+    @commands.has_permissions(administrator=True)
+    async def startgiveaway(self, ctx, time: int, prize: str, *roles: discord.Role):
+        if self.current_giveaway is not None:
+            await ctx.send("A giveaway is already running.")
+            return
 
-    @commands.command()
-    @is_owner_overridable()
-    async def myCom2(self, ctx):
-        await ctx.send("I still work!")
+        self.current_giveaway = {
+            "time": time,
+            "prize": prize,
+            "roles": roles,
+            "participants": []
+        }
+
+        embed = discord.Embed(title="Giveaway", description=f"React with 🎉 to enter the giveaway!\nPrize: {prize}")
+        embed.set_footer(text=f"Ends in {time} seconds.")
+
+        channel = self.bot.get_channel(self.giveaway_channel_id)
+        message = await channel.send(embed=embed)
+        await message.add_reaction("🎉")
+
+        await asyncio.sleep(time)
+        self.current_giveaway = None
+
+        new_message = await channel.fetch_message(message.id)
+        reaction = discord.utils.get(new_message.reactions, emoji="🎉")
+        participants = await reaction.users().flatten()
+
+        eligible_participants = [participant for participant in participants if any(role in participant.roles for role in roles)]
+
+        if eligible_participants:
+            winner = random.choice(eligible_participants)
+            await channel.send(f"Congratulations to {winner.mention} for winning the giveaway!")
+        else:
+            await channel.send("No eligible participants. The giveaway has ended.")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if self.current_giveaway is not None:
+            channel_id = payload.channel_id
+            message_id = payload.message_id
+            user_id = payload.user_id
+
+            if channel_id == self.giveaway_channel_id and message_id == self.current_giveaway["message_id"]:
+                guild = self.bot.get_guild(payload.guild_id)
+                member = guild.get_member(user_id)
+                
+                if any(role in member.roles for role in self.current_giveaway["roles"]):
+                    self.current_giveaway["participants"].append(member)
+
+def setup(bot):
+    bot.add_cog(Giveaway(bot))
