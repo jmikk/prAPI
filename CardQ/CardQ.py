5675915c23a4da8ea2c8666106fb833f78609762
jmikk <buldog.buldog26@gmail.com>
2023-06-09 16:08:29 -0500

    Update CardQ.py

--- CardQ/CardQ.py
+++ CardQ/CardQ.py
@@ -1,100 +1,47 @@
-from redbot.core import commands
-import asyncio
-import xml.etree.ElementTree as ET
 import discord
-
-
-def is_owner_overridable():
-    # Similar to @commands.is_owner()
-    # Unlike that, however, this check can be overridden with core Permissions
-    def predicate(ctx):
-        return False
-
-    return commands.permissions_check(predicate)
-
+from redbot.core import commands, Config
+import sqlite3
 
 class CardQ(commands.Cog):
-    """My custom cog"""
-    
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=1234567890)
+        default_global = {"database_path": "cards.db"}
+        self.config.register_global(**default_global)
 
-    # great once your done messing with the bot.
-    #   async def cog_command_error(self, ctx, error):
-    #       await ctx.send(" ".join(error.args))
-    
-    async def search_cards(self,xml_file, search_criteria):
-        with open(xml_file, "r", encoding="ISO-8859-1") as file:
-            xml_data = file.read()
-            xml_data = xml_data.replace("&", "&amp;")  # Replace & with &amp;
-
-        root = ET.fromstring(xml_data)
-
-
-        cards_found = []
-
-        for card in root.findall("SET/CARD"):
-            card_data = {"ID": card.find("ID").text, "NAME": card.find("NAME").text}
-            match_all_conditions = True
-
-            for criteria in search_criteria:
-                key, value = criteria.split(":", 1)
-                element = card.find(key.upper())
-
-                if element is not None and element.text == value:
-                    card_data[key] = element.text
-                elif key.upper() == "BADGES":
-                    badges = card.findall("BADGES/BADGE")
-                    badge_names = [
-                        badge.text
-                        for badge in badges
-                        if value.lower() in badge.text.lower()
-                    ]
-                    if badge_names:
-                        card_data[key] = badge_names
-                    else:
-                        match_all_conditions = False
-                elif key.upper() == "TROPHIES":
-                    trophies = card.findall("TROPHIES/TROPHY")
-                    trophy_names = [
-                        trophy.text
-                        for trophy in trophies
-                        if value.lower() in trophy.attrib.get("type", "").lower()
-                    ]
-                    if trophy_names:
-                        card_data[key] = trophy_names
-                    else:
-                        match_all_conditions = False
-                else:
-                    match_all_conditions = False
-
-            if match_all_conditions:
-                cards_found.append(
-                    card_data["NAME"]
-                    + ","
-                    + card_data["ID"]
-                    + ",3,www.nationstates.net/card="
-                    + card_data["ID"]
-                    + "/season=3\n"
-                )
-
-        return cards_found
-
-
-    # Example usage
     @commands.command()
-    async def card_search(self,ctx,Criteria):
-        await ctx.send("This will take some time theres a lot of cards!")
+    async def cardsearch(self, ctx, **kwargs):
+        # Retrieve the search criteria
+        search_criteria = kwargs
 
-        search_criteria={}
-        search_criteria = Criteria.split(",")
+        # Connect to the database
+        database_path = await self.config.database_path()
+        conn = sqlite3.connect(database_path)
+        cursor = conn.cursor()
 
-        found_cards = await self.search_cards("home/pi/cards.xml", search_criteria)
-        if len(found_cards) < 1:
-            await ctx.send("Found nothing")
-        with open("home/pi/output.txt", "w+") as f:
-            for card in found_cards:
-                f.write(card)
-                #await ctx.send(card)
-        with open("home/pi/output.txt","r") as f:
-            await ctx.send(file=discord.File(f))
+        # Build the SQL query dynamically based on the search criteria
+        sql_query = "SELECT * FROM cards WHERE "
+        sql_params = []
+        for key, value in search_criteria.items():
+            sql_query += f"{key} = ? AND "
+            sql_params.append(value)
+        sql_query = sql_query.rstrip(" AND ")
+
+        # Execute the query
+        cursor.execute(sql_query, sql_params)
+        results = cursor.fetchall()
+
+        # Display the results
+        if len(results) > 0:
+            for row in results:
+                # Format and send the card information as a Discord message
+                card_info = "\n".join(f"{key.capitalize()}: {value}" for key, value in zip(cursor.description, row))
+                await ctx.send(f"```{card_info}```")
+        else:
+            await ctx.send("No cards found matching the specified criteria.")
+
+        # Close the connection
+        conn.close()
+
+def setup(bot):
+    bot.add_cog(CardSearch(bot))
