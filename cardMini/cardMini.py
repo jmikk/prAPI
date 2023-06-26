4fc3e6b4ed21f81e6149922f2f2fd73080af06ba
jmikk <buldog.buldog26@gmail.com>
2023-06-26 11:31:47 -0500

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -36,6 +36,7 @@ class cardMini(commands.Cog):
                 avatar_hash = str(user.avatar) if user.avatar else str(user.default_avatar)
                 avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.png"
                 response = self.imgur_client.upload_from_url(avatar_url)
+                await ctx.send(response)
                 row["Flags"] = response["link"]
                 updated_rows.append(row)
             except Exception as e:
