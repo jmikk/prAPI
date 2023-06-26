4663bb2a084aae71c9ac33bda54961e309d1ec82
jmikk <buldog.buldog26@gmail.com>
2023-06-26 10:53:43 -0500

    Update CardQ.py

--- CardQ/CardQ.py
+++ CardQ/CardQ.py
@@ -95,7 +95,7 @@ class CardQ(commands.Cog):
                 sql_params.append(f"{value}%")
             elif key == "peflag":
                 sql_conditions.append(f"flag LIKE ?")
-                sql_params.append(f"{value}%")
+                sql_params.append(f"%{value}")
             else:
                 sql_conditions.append(f"LOWER({key}) = LOWER(?)")
                 sql_params.append(value)
