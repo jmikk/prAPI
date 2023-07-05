4b9c1a356243a773872999eaeac3246311ec7d0d
jmikk <buldog.buldog26@gmail.com>
2023-07-05 10:10:54 -0500

    Update CardQ.py

--- CardQ/CardQ.py
+++ CardQ/CardQ.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands
 import aiohttp
+import tempfile
 
 class CardQ(commands.Cog):
     def __init__(self, bot):
@@ -11,13 +12,11 @@ class CardQ(commands.Cog):
         base_url = "https://api.nsupc.dev/cards/v1"
         query_params = f"season={season}&{search_params}"
         search_url = f"{base_url}?{query_params}"
-        
-        # You can make an HTTP request to the API and process the response here
-        # Example using the aiohttp library:
+
         async with aiohttp.ClientSession() as session:
-             async with session.get(search_url) as response:
-                 data = await response.json()
-                 # Process the response data as needed
+            async with session.get(search_url) as response:
+                data = await response.json()
+                # Process the response data as needed
         
         # Generating the card list in the desired format
         card_list = []
@@ -25,8 +24,14 @@ class CardQ(commands.Cog):
             card_link = f"www.nationstates.net/page=deck/card={card_id}/season={season}"
             card_list.append(f"{card_id},{card_name},{card_link}")
         
+        # Creating a temporary file and writing the content to it
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_file:
+            tmp_file.write("\n".join(card_list))
+            tmp_file_path = tmp_file.name
+        
         # Creating and sending the file
-        file_content = "\n".join(card_list)
-        file = discord.File(filename="card_list.csv", data=file_content)
+        file = discord.File(tmp_file_path, filename="card_list.csv")
         await ctx.send(file=file)
 
+def setup(bot):
+    bot.add_cog(CardSearch(bot))
