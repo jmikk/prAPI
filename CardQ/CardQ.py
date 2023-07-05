90e10748969659565629c399d21f0fefde7c6e02
jmikk <buldog.buldog26@gmail.com>
2023-07-05 09:58:03 -0500

    Create CardQ.py

---
+++ CardQ/CardQ.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+class CardSearch(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def search_cards(self, ctx, season: int, *, search_params):
+        base_url = "https://api.nsupc.dev/cards/v1"
+        query_params = f"season={season}&{search_params}"
+        search_url = f"{base_url}?{query_params}"
+        
+        # You can make an HTTP request to the API and process the response here
+        # Example using the aiohttp library:
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get(search_url) as response:
+        #         data = await response.json()
+        #         # Process the response data as needed
+        
+        # Generating the card list in the desired format
+        card_list = []
+        for card_id, card_name in data["nations"].items():
+            card_link = f"www.nationstates.net/page=deck/card={card_id}/season={season}"
+            card_list.append(f"{card_id},{card_name},{card_link}")
+        
+        # Creating and sending the file
+        file_content = "\n".join(card_list)
+        file = discord.File(filename="card_list.csv", data=file_content)
+        await ctx.send(file=file)
+
+def setup(bot):
+    bot.add_cog(CardSearch(bot))
