26ccc1eda2e25c33deba24bb0718f4a52457d72f
jmikk <buldog.buldog26@gmail.com>
2023-07-16 17:57:13 -0500

    Update issues.py

--- issues/issues.py
+++ issues/issues.py
@@ -144,7 +144,7 @@ class issues(commands.Cog):
             if not self.stop_loop:
                 r = await self.api_request(data)
                 json_string = r.text
-                await ctx.send(len(json_string))
+                await ctx.send(json_string[0:1000])
                 data = json.loads(json_string)
 
                 embed = discord.Embed(
