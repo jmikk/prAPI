602305052f5aa76c821c0683b44ceb4d2d5adbd7
jmikk <buldog.buldog26@gmail.com>
2023-07-17 13:38:01 -0500

    Update NationCog.py

--- NationCog/NationCog.py
+++ NationCog/NationCog.py
@@ -21,6 +21,7 @@ class NationCog(commands.Cog):
                 missing_nations.append(nation)
 
         return missing_nations
+   
     @commands.command()
     async def ping_test(self,ctx,str=""):
         await ctx.send("This is a test <@&1130304387156279368>")
