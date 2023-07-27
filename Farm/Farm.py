4d6805a0f7b4f44fb5cbc4b15929c5853b06a8f0
jmikk <buldog.buldog26@gmail.com>
2023-07-26 20:58:11 -0500

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -23,8 +23,15 @@ class Farm(commands.Cog):
     # great once your done messing with the bot.
     #   async def cog_command_error(self, ctx, error):
     #       await ctx.send(" ".join(error.args))
-
+    
+    #https://www.quackit.com/character_sets/emoji/emoji_v3.0/unicode_emoji_v3.0_characters_food_and_drink.cfm
+    #🥔	POTATO	&#x1F954;
+    #🥕	CARROT	&#x1F955;
+    #🍄	MUSHROOM	&#x1F344;
+    #🌽	CORN &#x1F33D;
+    #🌮	TACO	&#x1F32E;
+    #🥑	AVOCADO	&#x1F951;
     @commands.command()
     @commands.is_owner()
-    async def myCom1(self, ctx):
-        await ctx.send("I work 🥔 🥕 🥦 🌽 🌮 🥑")
+    async def crops(self, ctx):
+        await ctx.send("The current crops you can grow are 🥔 (Potato) 🥕 (Carrot) 🍄 MUSHROOM 🌽(Corn) 🌮(Taco) 🥑 (Avacado)")
