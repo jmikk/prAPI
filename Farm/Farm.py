428059b48bdb541db462cbb283d031655f4a1a27
jmikk <buldog.buldog26@gmail.com>
2023-07-26 21:41:19 -0500

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -24,15 +24,52 @@ class Farm(commands.Cog):
     # great once your done messing with the bot.
     #   async def cog_command_error(self, ctx, error):
     #       await ctx.send(" ".join(error.args))
-    import sqlite3
+    
+    def make_new_player(player_id)
+        db_file = data_manager.cog_data_path(self) / f"players/{player_id}"  # Use data_manager.cog_data_path() to determine the database file path)
+        default_player_data = {
+        'level': 1,
+        'exp': 0,
+        'gold': 100,
+        'crop_rows': 1,
+        'crop_cols':1,
+        'potatoe': 0,
+        'carrot': 0,
+        'mushroom': 0,
+        'corn': 0,
+        'taco': 0,
+        'avocado': 0,
+        'potato_seeds': 10,
+        'carrot_seeds': 10,
+        'mushroom_seeds': 10,
+        'corn_seeds': 10,
+        'tacos_seeds': 10,
+        'avocados_seeds': 10
+    }
+        os.mkdir(db_file)
+        with open(db_file) as f:
+            f.write(default_player_data)
 
+
+    @commands.command()
+    def test(self,ctx):
+        make_new_player(ctx.author.id)
     # Function to initialize the database and create the player table
     @commands.command()
     @commands.is_owner()
-    async def initialize_database(self,ctx):
+    async def make_players_folder(self,ctx):
         db_file = data_manager.cog_data_path(self) / "players"  # Use data_manager.cog_data_path() to determine the database file path)
         os.mkdir(db_file)
         await ctx.send((os.path.exists(db_file) and os.path.isdir(db_file)))
+    
+    # Function to initialize the database and create the player table
+    @commands.command()
+    @commands.is_owner()
+    async def delete_players_folder(self,ctx):
+        db_file = data_manager.cog_data_path(self) / "players"  # Use data_manager.cog_data_path() to determine the database file path)
+        os.rmdir(db_file)
+        await ctx.send((os.path.exists(db_file) and os.path.isdir(db_file)))
+    
         
     #https://www.quackit.com/character_sets/emoji/emoji_v3.0/unicode_emoji_v3.0_characters_food_and_drink.cfm
     #🥔	POTATO	&#x1F954;
@@ -42,6 +79,5 @@ class Farm(commands.Cog):
     #🌮	TACO	&#x1F32E;
     #🥑	AVOCADO	&#x1F951;
     @commands.command()
-    @commands.is_owner()
     async def crops(self, ctx):
         await ctx.send("The current crops you can grow are 🥔 (Potato) 🥕 (Carrot) 🍄 MUSHROOM 🌽(Corn) 🌮(Taco) 🥑 (Avacado)")
