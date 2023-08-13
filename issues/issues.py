be7e68e99baa4402698bb89a1262d78056743de9
jmikk <buldog.buldog26@gmail.com>
2023-08-13 18:17:02 -0500

    Update issues.py

--- issues/issues.py
+++ issues/issues.py
@@ -202,12 +202,12 @@ class issues(commands.Cog):
 
                 await channel_out.send(embed=embed)
                 self.auth = sans.NSAuth(password=self.password)
-                str = f"Fresh from the well, \n  "+desc_element.text.replace("<i>","*").replace("</i>","*").replace("<b>","**").replace("</b>","**")+" \n If you would like to help decide my fate join our discord where you can vote every 12 hours."
+                str2 = f"Fresh from the well, \n  "+desc_element.text.replace("<i>","*").replace("</i>","*").replace("<b>","**").replace("</b>","**")+" \n If you would like to help decide my fate join our discord where you can vote every 12 hours."
                 data = {
                     "nation": self.RegionalNation,
                     "region": Region,
                     "c": "rmbpost",
-                    "text": str,
+                    "text": str2,
                     "mode": "prepare",
                 }
                 r = await self.api_request(data=data)
