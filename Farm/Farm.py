201f86daa9948b223cd5fd27880acb850b659125
jmikk <buldog.buldog26@gmail.com>
2023-08-13 21:32:46 -0500

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -1,79 +1,74 @@
-from redbot.core import commands,data_manager
+from redbot.core import commands,config
 import discord
 import datetime
+import sqlite3
+
 
 class Farm(commands.Cog):
     """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=1234567890)
+        default_global = {
+            "players": {}  # Dictionary to store player data
+        }
+        self.config.register_global(**default_global)
+    async def get_player_data(self, player_id: int):
+    players = await self.config.players()
+    return players.get(player_id, {})
 
-    # Store user data and their crops
-    user_data = {}
-    
-    # Crop growth time (in days)
-    crop_growth_time = {
-        "POTATO": 2,
-        "CARROT": 3,
-        "MUSHROOM": 4,
-        "CORN": 5,
-        "TACO": 6,
-        "AVOCADO": 7
-    }
-    
-    @commands.command()
-    async def update_growths(user_id):
-        now = datetime.datetime.now()
-        for crop_name, data in user_data[user_id].items():
-            last_action_time = data["last_action_time"]
-            days_since_last_action = (now - last_action_time).days
-            if days_since_last_action > 0:
-                data["growth_progress"] += days_since_last_action
-                data["last_action_time"] = now
-    
-                if data["growth_progress"] >= crop_growth_time[crop_name]:
-                    data["growth_progress"] = 0
-                    data["ready_to_harvest"] = True
+    async def set_player_data(self, player_id: int, data: dict):
+        players = await self.config.players()
+        players[player_id] = data
+        await self.config.players.set(players)
 
     @commands.command()
-    async def plant(ctx, crop_name):
-        user_id = str(ctx.author.id)
-        if user_id not in user_data:
-            user_data[user_id] = {}
-    
-        if crop_name in crop_growth_time:
-            if crop_name not in user_data[user_id]:
-                user_data[user_id][crop_name] = {
-                    "growth_progress": 0,
-                    "ready_to_harvest": False,
-                    "last_action_time": datetime.datetime.now()
-                }
-            await ctx.send(f"You planted {crop_name}!")
-            update_growths(user_id)
-        else:
+    async def inventory(self, ctx):
+        player_id = ctx.author.id
+        player_data = await self.get_player_data(player_id)
+        inventory = player_data.get("inventory", {})
+        
+        if not inventory:
+            await ctx.send("Your inventory is empty.")
+            return
+        
+        inventory_text = "\n".join([f"{item}: {count}" for item, count in inventory.items()])
+        await ctx.send(f"Your inventory:\n{inventory_text}")
+
+    @commands.command()
+    async def plant(self, ctx, crop: str):
+        player_id = ctx.author.id
+        crop = crop.lower()
+        
+        if crop not in ["potato", "carrot", "mushroom", "corn", "taco", "avocado"]:
             await ctx.send("Invalid crop name.")
-    
-    @commands.command()
-    async def harvest(ctx, crop_name):
-        user_id = str(ctx.author.id)
-        if user_id in user_data and crop_name in user_data[user_id]:
-            if user_data[user_id][crop_name]["ready_to_harvest"]:
-                user_data[user_id][crop_name]["ready_to_harvest"] = False
-                await ctx.send(f"You harvested {crop_name}!")
-            else:
-                await ctx.send(f"{crop_name} is not ready for harvest.")
+            return
+        
+        player_data = await self.get_player_data(player_id)
+        inventory = player_data.get("inventory", {})
+        
+        if inventory.get(crop, 0) > 0:
+            inventory[crop] -= 1
+            await self.set_player_data(player_id, {"inventory": inventory})
+            
+            await ctx.send(f"You planted a {crop}!")
         else:
-            await ctx.send("You don't have any of that crop to harvest.")
-    
+            await ctx.send(f"You don't have any {crop} to plant.")
     @commands.command()
-    async def status(ctx):
-        user_id = str(ctx.author.id)
-        if user_id in user_data:
-            status_message = "Your farm status:\n"
-            for crop_name, data in user_data[user_id].items():
-                growth_progress = data["growth_progress"]
-                ready_to_harvest = "Ready to harvest" if data["ready_to_harvest"] else f"Growth: {growth_progress}/{crop_growth_time[crop_name]}"
-                status_message += f":{crop_name}: {ready_to_harvest}\n"
-            await ctx.send(status_message)
+    async def harvest(self, ctx, crop: str):
+        player_id = ctx.author.id
+        crop = crop.lower()
+        
+        player_data = await self.get_player_data(player_id)
+        inventory = player_data.get("inventory", {})
+        
+        if crop in inventory:
+            inventory[crop] += 1
         else:
-            await ctx.send("You haven't started farming yet.")
+            inventory[crop] = 1
+        
+        await self.set_player_data(player_id, {"inventory": inventory})
+        await ctx.send(f"You harvested a {crop}!")
+            
+    
