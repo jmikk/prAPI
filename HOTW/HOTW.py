67028a53dd6e65abb30e60648f2167c51d1d9bdf
jmikk <buldog.buldog26@gmail.com>
2023-09-29 19:20:52 -0500

    Update HOTW.py

--- HOTW/HOTW.py
+++ HOTW/HOTW.py
@@ -125,7 +125,7 @@ class HOTW(commands.Cog):
     "Bob procures Fancy Water from Joe in a stealthy manner."
 ]
         random_statement = random.choice(ways_to_take_water)
-        random_statement = random_statement.replace("Bob",ctx.author.mention).replace("Joe",HOTW)
+        random_statement = random_statement.replace("Bob",str(ctx.author.mention)).replace("Joe",str(HOTW))
         current_time = datetime.now()
         given_datetime = datetime.fromtimestamp(timestamp)
         time_difference_seconds = current_epoch_timestamp - given_epoch_timestamp
