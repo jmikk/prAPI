87dd85b76fba1bee71d4d4dbda0e7611966d99ad
jmikk <buldog.buldog26@gmail.com>
2023-10-16 19:53:43 -0500

    Update prAPI.py

--- RN/prAPI.py
+++ RN/prAPI.py
@@ -72,9 +72,11 @@ class prAPI(commands.Cog):
                 "mode": "prepare",
             }
             r = await self.api_request(data=data)
+            await ctx.send(r.text)
             rmbToken = r.xml.find("SUCCESS").text
             data.update(mode="execute", token=rmbToken)
             r = await self.api_request(data=data)
+            await ctx.send(r.text)
             #await ctx.send(r.text)
             await ctx.send(f"Posted on  {Region} RMB")
             await asyncio.sleep(self.QOTDTime)
