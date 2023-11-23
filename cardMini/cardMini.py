33e63bf1a1adec288f7045882991322620298cd3
jmikk <buldog.buldog26@gmail.com>
2023-11-23 12:15:12 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -81,7 +81,7 @@ class cardMini(commands.Cog):
             cursor = conn.cursor()
             
             cursor.execute(f'''
-                CREATE TABLE IF NOT EXISTS bank_{id} (
+                CREATE TABLE IF NOT EXISTS {"bank_"+str(id)} (
                     userID INTEGER PRIMARY KEY DEFAULT {id},
                     bank REAL DEFAULT 0.1
                 )
