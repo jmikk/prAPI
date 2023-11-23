bea3e80ef7150050d291aa85da3195c1edeb32cc
jmikk <buldog.buldog26@gmail.com>
2023-11-23 10:36:19 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -116,9 +116,9 @@ class cardMini(commands.Cog):
                     # Use a parameterized query to retrieve all elements from the table
                     cursor.execute(f'SELECT * FROM {row[1]} WHERE userID = ?',(row[0],))                    
                     # Fetch all the rows from the result set
-                    rows = cursor.fetchall()
+                    rowz = cursor.fetchall()
 
-                    embed.add_field(name=f"Card name: {name}", value=f"{row[1]}, You own: {row[2]} ID: {row[0]} Rarity: {row[4]}\nMV: {row[3]} Buy price: {round(row[3]*buy_mod,2)} Sell price: {round(row[3]*sell_mod,2)}", inline=False)
+                    embed.add_field(name=f"Card name: {name}", value=f"{row[1]}, You own: {row[2]} ID: {row[0]} Rarity: {rowz[2]}\nMV: {rowz[3]} Buy price: {round(rowz[3]*buy_mod,2)} Sell price: {round(rowz[3]*sell_mod,2)}", inline=False)
 
                     #embed.add_field(name=f"Card name: {name}", value=f"{row[1]}, You own: {row[2]} ID: {row[0]} Rarity: {row[2]}\nMV: {row[3]} Buy price: {round(row[3]*buy_mod,2)} Sell price: {round(row[3]*sell_mod,2}", inline=False)
 
