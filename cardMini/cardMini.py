b14abc63b2a9c6de6e94c82daf7ac47fd45d9c9e
jmikk <buldog.buldog26@gmail.com>
2023-12-03 12:49:32 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -129,6 +129,10 @@ class cardMini(commands.Cog):
             cursor.execute(f'''
                 SELECT MV FROM {series} WHERE name = ?
             ''', (name,))
+            
+            cursor.execute(f"SELECT userID FROM {series} WHERE name = ?", (card_name,))
+            userID = cursor.fetchone()
+
     
             # Fetch the result
             result = cursor.fetchone()
@@ -169,11 +173,11 @@ class cardMini(commands.Cog):
                         # If the user and season combination exists, update the count
                         new_count = result2[2] + 1
                         update_query = f"UPDATE {table_name} SET count = ? WHERE userID = ? AND season = ?"
-                        cursor.execute(update_query, (int(new_count), result[0], series))
+                        cursor.execute(update_query, (int(new_count), userID[0], series))
                     else:
                         # If the user and season combination doesn't exist, insert a new record
                         insert_query = f"INSERT INTO {table_name} (userID, season, count) VALUES (?, ?, ?)"
-                        cursor.execute(insert_query, (int(result[0]), series, 1))   
+                        cursor.execute(insert_query, (userID[0], series, 1))   
                     # Commit the changes
                     conn.commit()
 
