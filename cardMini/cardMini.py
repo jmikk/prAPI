5884794cd5df5339c715a26993ce8dc9e917a4c1
jmikk <buldog.buldog26@gmail.com>
2023-12-03 14:10:20 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -136,9 +136,9 @@ class cardMini(commands.Cog):
             cursor.execute(f"SELECT userID FROM {series} WHERE name = ?", (name,))
             userID = cursor.fetchone()
 
-           if stock <= 0:
-               await ctx.send("I don't have a copy of that card but sometimes when you try and open a pack I get a card!")
-               return
+            if MV[1] <= 0:
+                await ctx.send("I don't have a copy of that card but sometimes when you try and open a pack I get a card!")
+                return
                
     
             if MV:
