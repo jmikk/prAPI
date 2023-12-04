d463dcabceba6c8ba22c6ab75504e4f7e50b0641
jmikk <buldog.buldog26@gmail.com>
2023-12-03 19:33:16 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -96,8 +96,8 @@ class cardMini(commands.Cog):
         embed.add_field(name="MV", value=card[4], inline=True)
         embed.add_field(name="Gob owns", value=card[5], inline=True)
         embed.add_field(name="You own", value=owner_count[0], inline=True)
-        embed.add_field(name="Buy Price", value=round(float(card[4])*self.buy_mod,2), inline=True)
-        embed.add_field(name="Sell Price", value=round(float(card[4])*self.sell_mod+.01,2), inline=True)
+        embed.add_field(name="Gob will buy for", value=round(float(card[4])*self.buy_mod,2), inline=True)
+        embed.add_field(name="Gob will sell for", value=round(float(card[4])*self.sell_mod+.01,2), inline=True)
             
         # Set the thumbnail to the user's avatar if available, otherwise use the default icon
         avatar_url = user.avatar.url if user.avatar else user.default_avatar.url
@@ -553,8 +553,8 @@ class cardMini(commands.Cog):
                 embed.add_field(name="MV", value=card[4], inline=True)
                 embed.add_field(name="Gob owns", value=card[5], inline=True)
                 embed.add_field(name="You own", value=owner_count[0], inline=True)
-                embed.add_field(name="Buy Price", value=round(float(card[4])*self.buy_mod,2), inline=True)
-                embed.add_field(name="Sell Price", value=round(float(card[4])*self.sell_mod+.01,2), inline=True)
+                embed.add_field(name="Gob will buy for", value=round(float(card[4])*self.buy_mod,2), inline=True)
+                embed.add_field(name="Gob will sell for", value=round(float(card[4])*self.sell_mod+.01,2), inline=True)
             
                 # Set the thumbnail to the user's avatar if available, otherwise use the default icon
                 avatar_url = user.avatar.url if user.avatar else user.default_avatar.url
