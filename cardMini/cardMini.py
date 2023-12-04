a05d6128332b7177c0e5cdc13e9e33ebf1bb53d8
jmikk <buldog.buldog26@gmail.com>
2023-12-03 19:59:44 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -19,6 +19,7 @@ class cardMini(commands.Cog):
     @commands.command(name='mine_salt')
     async def mine_salt(self,ctx):
         server_id = str(ctx.guild.id)
+        os.path.join(data_manager.cog_data_path(self), "bad_stuff.txt")
         db_path = os.path.join(data_manager.cog_data_path(self), f'{server_id}.db')
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
