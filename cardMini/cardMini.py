1323f71937d4d619fc1dec29e5077e9d908b23e4
jmikk <buldog.buldog26@gmail.com>
2023-12-06 13:42:41 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -18,7 +18,10 @@ class cardMini(commands.Cog):
         self.buy_mod=.9
 
     @commands.command(name='gob_pack')
-    async def gob_pack(self,server_id,db_path):
+    async def gob_pack(self,ctx):
+        server_id = str(ctx.guild.id)
+
+        db_path = os.path.join(data_manager.cog_data_path(self), f'{server_id}.db')
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
         # Retrieve a random user from the specified series
