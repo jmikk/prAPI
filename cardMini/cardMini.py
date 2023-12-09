a9bf191aa33ba2db75b42b80fe2454eb26e3b26c
jmikk <buldog.buldog26@gmail.com>
2023-12-09 13:42:42 -0600

    Update cardMini.py

--- cardMini/cardMini.py
+++ cardMini/cardMini.py
@@ -949,7 +949,7 @@ class cardMini(commands.Cog):
         self.steal_mod=percent
     
     @commands.command(name='random_user',aliases=["open","open_pack"])
-    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.cooldown(1, 5, commands.BucketType.user)
     async def random_user(self, ctx):
         """Select a random user from the specified series and add their ID to the user's deck."""
 
