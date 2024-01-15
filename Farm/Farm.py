a64e9cc45d32b1c249ba7f0d07606b7d46681d75
jmikk <buldog.buldog26@gmail.com>
2024-01-15 17:03:06 -0600

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -32,7 +32,7 @@ class Farm(commands.Cog):
                            (player_id, str(initial_inventory)))
             self.conn.commit()
             return await self.get_player_data(player_id,depth=1)
-        return {} if result is None else {"inventory": eval(result[1])}
+        return {} if result is None else {"inventory": result}
 
     async def set_player_data(self, player_id: int, data: dict):
         cursor = self.conn.cursor()
