9e1459d640904fc453d1bc05686d95eb6b9bc909
jmikk <buldog.buldog26@gmail.com>
2024-01-31 07:28:19 -0600

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -14,7 +14,7 @@ class Farm(commands.Cog):
         default_user = {
             "inventory": {},
             "fields": {},
-            "gold": 0
+            "gold": 0,
             "field_size": 1  # Default field size allowing only 1 crop at a time
         }
         self.config.register_user(**default_user)
