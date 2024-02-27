0cf2bee5ae3dbef17ac8fdf855c3d1510fa78731
jmikk <buldog.buldog26@gmail.com>
2024-02-27 08:15:38 -0600

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -196,12 +196,13 @@ class Farm(commands.Cog):
             loot_box_item = random.choice(loot_items)
             item_name = loot_box_item['name']
             stats = loot_box_item['stats']
-    
+
+            await ctx.send(f"Congratulations! You've received a {item_name} from the loot box! Stats: {stats}")
+
             # Add the loot item to the player's inventory
             await self._add_loot_to_inventory(ctx,ctx.author, loot_box_item, stats)
     
             # Inform the player about their loot
-            await ctx.send(f"Congratulations! You've received a {item_name} from the loot box! Stats: {stats}")
 
     
     async def _add_loot_to_inventory(self,ctx, user, item, stats):
