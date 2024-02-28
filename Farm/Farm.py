9bf86ca1f371f78f7fce257e0201ee79bcb7369a
jmikk <buldog.buldog26@gmail.com>
2024-02-28 13:39:58 -0600

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -245,7 +245,7 @@ class Farm(commands.Cog):
                     
                     
                     # Add the new item's stats
-                    for  stat, bonus in new_item_stats.split(", "):
+                    for  stat, bonus in stats:
                         await ctx.send(str(stat) + str(bonus))
                         if stat in user_data:
                             user_data[stat] = user_data[stat] + bonus
