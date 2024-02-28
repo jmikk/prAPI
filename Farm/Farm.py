5d97c00dc88c698bb07f39517b507d435681c143
jmikk <buldog.buldog26@gmail.com>
2024-02-28 14:12:15 -0600

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -695,7 +695,7 @@ class Farm(commands.Cog):
         life  = await self.config.user(ctx.author).Health()
         crit = await self.config.user(ctx.author).Critical_chance()
 
-        await ctx.send(f"Your defense is {defense}\n Your rep is {rep}\n Your Speed is: {speed}\n Your luck is {luck}/n Your Life is {life}/n Your crit chance is {crit}")
+        await ctx.send(f"Your defense is {defense} \n Your rep is {rep} \n Your Speed is: {speed} \n Your luck is {luck} \n Your Life is {life} \n Your crit chance is {crit}")
 
             
     
