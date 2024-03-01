6233277fd297a7305656da5c031fa097797bfb7d
jmikk <buldog.buldog26@gmail.com>
2024-03-01 08:36:33 -0600

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -194,7 +194,7 @@ class Farm(commands.Cog):
             enemy_effective_defense = enemy_stats['defense'] * (1 + enemy_stats['speed'] / 100)
     
             player_damage = max(round_count, player_effective_attack - enemy_effective_defense)
-            enemy_damage = max(round_count*round_count, enemy_effective_attack - player_effective_defense)
+            enemy_damage = max(round_count * round_count, enemy_effective_attack - player_effective_defense)
     
             # Consider Critical Chance
             if random.random() < user_data['Critical_chance'] / 100:
