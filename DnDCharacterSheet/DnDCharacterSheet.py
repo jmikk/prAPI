a263d588fa04123389b04b18b662a308ca986811
jmikk <buldog.buldog26@gmail.com>
2024-03-28 15:55:37 -0500

    Update DnDCharacterSheet.py

--- DnDCharacterSheet/DnDCharacterSheet.py
+++ DnDCharacterSheet/DnDCharacterSheet.py
@@ -23,7 +23,7 @@ class PotionView(View):
         potion_name, potion_details = self.potions[self.current_index]
         effects_text = "\n".join(f"{effect['name']}: {effect['text']}" for effect in potion_details['effects'])
         self.embed = Embed(title=f"{potion_name} (Quantity: {potion_details['quantity']})", description=effects_text, color=Color.blue())
-        return True
+        self.send_current_embed()
 
     async def send_current_embed(self):
         if hasattr(self, 'message'):
@@ -44,7 +44,7 @@ class PotionView(View):
     async def next(self, button: Button, interaction: Interaction):
         self.current_index = (self.current_index + 1) % len(self.potions)
         self.update_embed()
-        await self.send_current_embed()
+        #await self.send_current_embed()
 
     @discord.ui.button(label="Drink", style=ButtonStyle.green)
     async def drink(self, interaction: Interaction, button: Button):
