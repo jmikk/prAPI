e05182a278c7c28f2e6c7ba19b327e837e0edbf2
jmikk <buldog.buldog26@gmail.com>
2024-03-29 15:02:03 -0500

    Update DnDCharacterSheet.py

--- DnDCharacterSheet/DnDCharacterSheet.py
+++ DnDCharacterSheet/DnDCharacterSheet.py
@@ -97,7 +97,7 @@ class GuildStashView(ui.View):
 
         # Confirm the action to the user
         await interaction.response.edit_message(embed=self.embed, view=self)
-        await interaction.followup.send(f"You took one {potion_name} from the guild stash.", ephemeral=True)
+        await interaction.followup.send(f"{self.member.mention} took one {potion_name} from the guild stash.")
 
 
 
