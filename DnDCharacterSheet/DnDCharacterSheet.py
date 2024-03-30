a3dc4cfdac6d579b6b2bb567e6cb8c3b31c180ae
jmikk <buldog.buldog26@gmail.com>
2024-03-30 12:47:37 -0500

    Update DnDCharacterSheet.py

--- DnDCharacterSheet/DnDCharacterSheet.py
+++ DnDCharacterSheet/DnDCharacterSheet.py
@@ -681,7 +681,7 @@ class DnDCharacterSheet(commands.Cog):
     @dnd.command()
     async def create_character(self, ctx: commands.Context):
         """Sends a button to the user to create a new D&D character sheet."""
-        view = CharacterSheetView(self, cog)
+        view = CharacterSheetView(self, ctx)
         await ctx.send("Click the button below to create your D&D character sheet:", view=view)
 
 
