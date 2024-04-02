6829bfbfe6c4272f984b89df50aba907a1f78a33
jmikk <buldog.buldog26@gmail.com>
2024-04-01 19:36:17 -0500

    Update Recruitomatic9006.py

--- Recruitomatic9006/Recruitomatic9006.py
+++ Recruitomatic9006/Recruitomatic9006.py
@@ -59,13 +59,14 @@ class Recruitomatic9006(commands.Cog):
                 nations.append(newnation.get("name"))
         return nations
 
-    def generate_telegram_urls(self, nations):
+   async def generate_telegram_urls(self, nations):
         """Generates URLs for sending telegrams to nations, in chunks of 8."""
         urls = []
         base_url = "https://www.nationstates.net/page=compose_telegram?tgto="
         template = "&message=%25TEMPLATE-29841116%25"
 
         for i in range(0, len(nations), 8):
+            await self.target_channel.send("loop")
             nation_chunk = nations[i:i+8]
             urls.append(base_url + ",".join(nation_chunk) + template)
 
