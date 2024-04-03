c0c947e48f2efdfb9ce7c1e5e088d8b87311acff
jmikk <buldog.buldog26@gmail.com>
2024-04-03 17:23:54 -0500

    Update Recruitomatic9006.py

--- Recruitomatic9006/Recruitomatic9006.py
+++ Recruitomatic9006/Recruitomatic9006.py
@@ -74,6 +74,8 @@ class ApproveButton(Button):
             # Send the feedback embed with the new view as a follow-up
             for each in self.nations_list:
                 nations_tged.append(each)
+            await interaction.response.send_message("here", ephemeral=True)
+
             await interaction.followup.send(nations_tged, view=view)
             await interaction.followup.send(embed=embed, view=view)
 
