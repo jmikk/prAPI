cc299a1318a6035eda0bd13c674c58e4ee86a322
jmikk <buldog.buldog26@gmail.com>
2024-04-05 16:06:20 -0500

    Update Recruitomatic9006.py

--- Recruitomatic9006/Recruitomatic9006.py
+++ Recruitomatic9006/Recruitomatic9006.py
@@ -109,7 +109,7 @@ class DoneButton(Button):
     
                 # Stop the recruitment loop
                 self.cog_instance.loop_running = False
-                await Recruitomatic9006.send_nations_file(self.ctx)
+                await Recruitomatic9006.send_nations_file(self, self.ctx)
                 # Fetch the total tokens and send a follow-up message with the embed
             else:
                 # If the user is not the invoker, send an error message
