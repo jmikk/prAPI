f5db1b4012a93165ac629a2be834ea5ee76ac968
jmikk <buldog.buldog26@gmail.com>
2024-04-13 16:51:35 -0500

    Update DisWonder.py

--- DisWonder/DisWonder.py
+++ DisWonder/DisWonder.py
@@ -73,7 +73,7 @@ class DisWonder(commands.Cog):
         # Example logic for modifying item quantities
         if tokens > 0:
             # Select a random item to increment
-            user_data[recipe_result] = user_data.get(recipe_result, 0) + 1
+            user_data[chosen_item] = user_data.get(chosen_item, 0) + 1
             # Save the updated items back to the user's config
             await ctx.send(f"You spent {tokens} tokens and received {tokens} units of {chosen_item}.")
             await self.config.user(ctx.author).set(user_data)
