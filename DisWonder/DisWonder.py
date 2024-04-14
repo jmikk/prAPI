086c241b25a0f4d07fb426d54c8fe5e131b9fc20
jmikk <buldog.buldog26@gmail.com>
2024-04-14 09:40:53 -0500

    Update DisWonder.py

--- DisWonder/DisWonder.py
+++ DisWonder/DisWonder.py
@@ -41,8 +41,8 @@ class CraftingView(discord.ui.View):
         filtered_items = {k: v for k, v in user_data.items() if k.lower().endswith(mini_item_type) and v > 0}
 
         if filtered_items:
-            self.add_item(ItemSelect(filtered_items, custom_id="item1"))
-            self.add_item(ItemSelect(filtered_items, custom_id="item2"))
+            self.add_item(ItemSelect(filtered_items))
+            self.add_item(ItemSelect(filtered_items))
         else:
             asyncio.create_task(ctx.send("No items available to craft this type of product."))
 
