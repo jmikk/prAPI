7114320a5b1f4d3067ddfd5d2254eb35ec93116d
jmikk <buldog.buldog26@gmail.com>
2024-04-15 10:02:36 -0500

    Update DisWonder.py

--- DisWonder/DisWonder.py
+++ DisWonder/DisWonder.py
@@ -9,8 +9,9 @@ import asyncio
 
 
 
+
 class CraftButton(discord.ui.Button):
-    def __init__(self, label,quantity):
+    def __init__(self, label, quantity):
         super().__init__(label=label, style=discord.ButtonStyle.green)
         self.quantity = quantity  # The quantity to craft when this button is pressed
 
