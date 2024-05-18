f0f15e9964bc2c8b9026a918af9d4573fa8d6bb8
jmikk <buldog.buldog26@gmail.com>
2024-05-17 19:54:22 -0500

    Update sheets.py

--- sheets/sheets.py
+++ sheets/sheets.py
@@ -62,7 +62,6 @@ class sheets(commands.Cog):
         
         try:
             xml_list = xml_content.split("\n")
-            await ctx.send(xml_list)
             for each in xml_list:
                 await ctx.send(each)
                 if each.startswith("<CARDID>"):
