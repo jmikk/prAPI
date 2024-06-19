1223b4be983a02431ce8768703e03ea388f5be2b
jmikk <buldog.buldog26@gmail.com>
2024-06-19 15:57:28 -0500

    Update lootbox.py

--- lootbox/lootbox.py
+++ lootbox/lootbox.py
@@ -6,7 +6,7 @@ from redbot.core.bot import Red
 from discord import Embed
 import time
 
-class Lootbox(commands.Cog):
+class lootbox(commands.Cog):
     def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)
