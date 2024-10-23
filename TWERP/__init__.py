8d5380234d864594f441ba1a21a9fab973c53e96
jmikk <buldog.buldog26@gmail.com>
2024-10-23 14:49:51 -0500

    Update __init__.py

--- TWERP/__init__.py
+++ TWERP/__init__.py
@@ -3,7 +3,7 @@ from redbot.core import commands
 
 @commands.Cog.listener()
 async def on_ready(self):
-    guild_id = YOUR_GUILD_ID  # Replace with your guild ID if you want to test commands in a specific server
+    guild_id = 1098644885797609492  # Replace with your guild ID if you want to test commands in a specific server
     guild = discord.Object(id=guild_id)
     await self.bot.tree.sync(guild=guild)
     print("Commands synced!")
