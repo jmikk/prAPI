1390e7844335a6aa0a0bd1ea8223627419ece824
jmikk <buldog.buldog26@gmail.com>
2024-10-23 14:48:05 -0500

    Update __init__.py

--- TWERP/__init__.py
+++ TWERP/__init__.py
@@ -1,5 +1,13 @@
 from .TWERP import TWERP
 
 
+@commands.Cog.listener()
+async def on_ready(self):
+    guild_id = YOUR_GUILD_ID  # Replace with your guild ID if you want to test commands in a specific server
+    guild = discord.Object(id=guild_id)
+    await self.bot.tree.sync(guild=guild)
+    print("Commands synced!")
+
+
 async def setup(bot):
     await bot.add_cog(TWERP(bot))
