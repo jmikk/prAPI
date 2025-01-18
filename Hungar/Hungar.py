047dddb2bda30bdd1d034abe71d6f5841e4eafe8
jmikk <buldog.buldog26@gmail.com>
2025-01-17 18:23:17 -0600

    Update Hungar.py

--- Hungar/Hungar.py
+++ Hungar/Hungar.py
@@ -266,6 +266,7 @@ class Hungar(commands.Cog):
     
     async def cog_load(self):
         """This method is called when the cog is loaded, and it ensures that all slash commands are synced."""
+        self.bot.tree.add_command(self.sponsor)
         # Sync the commands globally
         await self.bot.tree.sync()
         print("All slash commands synced.")
