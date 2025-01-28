7dfbf6ea224f6aed40322f6eb98773ec2f727613
jmikk <buldog.buldog26@gmail.com>
2025-01-27 18:48:40 -0600

    Update Hungar.py

--- Hungar/Hungar.py
+++ Hungar/Hungar.py
@@ -1108,6 +1108,7 @@ class Hungar(commands.Cog):
         day_start = datetime.utcnow()
         alive_count = len(alive_players)
         day_duration = max(int(alive_count) * 20 + 20,60)  
+        day_counter = config.get("day_counter", 0)
         if day_counter > 0 and day_counter % 10 == 0:
             day_duration = int(day_duration * 1.5)  # Feast days are longer
     
