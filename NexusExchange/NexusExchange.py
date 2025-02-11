4e3f0a19b41351f6eda87f910435933a52fab360
jmikk <buldog.buldog26@gmail.com>
2025-02-10 20:10:41 -0600

    Update NexusExchange.py

--- NexusExchange/NexusExchange.py
+++ NexusExchange/NexusExchange.py
@@ -265,11 +265,13 @@ class NexusExchange(commands.Cog):
                 card_info_data = await response.text()
                 card_info = self.parse_card_info(card_info_data)
 
-                embed_color = self.get_embed_color(random_card['category'])
+                root = ET.fromstring(card_info_data)
+
+                embed_color = self.get_embed_color(root.find('category'))
                 embed = Embed(title="Loot Box Opened!", description="You received a card!", color=embed_color)
                 embed.add_field(name="Card Name", value=card_info['name'], inline=True)
-                embed.add_field(name="Card ID", value=random_card['id'], inline=True)
-                embed.add_field(name="Season", value=random_card['season'], inline=True)
+                embed.add_field(name="Card ID", value=id, inline=True)
+                embed.add_field(name="Season", value=4, inline=True)
                 embed.add_field(name="Market Value", value=card_info['market_value'], inline=True)
 
                 # Prepare the gift
