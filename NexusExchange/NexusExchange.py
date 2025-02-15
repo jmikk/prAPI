ebcbab941aad6e43c16117c9dc3f9ac53e58eec0
jmikk <buldog.buldog26@gmail.com>
2025-02-15 14:03:42 -0600

    Update NexusExchange.py

--- NexusExchange/NexusExchange.py
+++ NexusExchange/NexusExchange.py
@@ -177,8 +177,8 @@ class NexusExchange(commands.Cog):
         password = await self.config.password()  # Nation's password
         useragent = await self.config.useragent()  # Custom user agent
 
-        dispatch_content = dispatch_content.encode("utf-8").decode("utf-8")        
-        
+        dispatch_content = html.escape(dispatch_content)        
+       
         url = "https://www.nationstates.net/cgi-bin/api.cgi"
         headers = {
             "User-Agent": useragent,
