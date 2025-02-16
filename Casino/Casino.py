53d56af6a15514669a04079258aa27a1bae32833
jmikk <buldog.buldog26@gmail.com>
2025-02-16 11:02:26 -0600

    Update Casino.py

--- Casino/Casino.py
+++ Casino/Casino.py
@@ -191,6 +191,7 @@ class Casino(commands.Cog):
         """Display statistics of the last 100 roulette rolls."""
         if not self.roulette_history:
             return await ctx.send("No rolls recorded yet.")
+
         
         red_numbers = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}
         black_numbers = {2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35}
