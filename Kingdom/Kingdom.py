8f60a1358752aab3c076686e1feae13fb38224fa
jmikk <buldog.buldog26@gmail.com>
2025-02-18 17:57:57 -0600

    Update Kingdom.py

--- Kingdom/Kingdom.py
+++ Kingdom/Kingdom.py
@@ -359,6 +359,7 @@ class Kingdom(commands.Cog):
                 incomplete_projects.append(project)
             else:
                 debug_message += f"\nSkipping {project['name']} - Missing prerequisites: {missing_prereqs}"
+        await user.send(completed_projects)
         await user.send(debug_message)  # Sends debugging info to the user
         return incomplete_projects
 
