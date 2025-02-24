c1c945e9dce7ccd799fff605847d8468762ef563
jmikk <buldog.buldog26@gmail.com>
2025-02-24 16:13:13 -0600

    Update NexusExchange.py

--- NexusExchange/NexusExchange.py
+++ NexusExchange/NexusExchange.py
@@ -175,9 +175,8 @@ class NexusExchange(commands.Cog):
         total_buttons = 0
 
         for days_required, code in tg_data.items():
-            await ctx.send(days_required)
-            await ctx.send(code)
             nations_to_send = [nation for nation, days in nation_data.items() if days == days_required]
+            ctx.send(len(nations_to_send)
 
             if not nations_to_send:
                 continue  # Skip if no nations match the criteria
