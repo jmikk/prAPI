20c54566563722d3066358f2321c6b83d5e243b0
jmikk <buldog.buldog26@gmail.com>
2025-03-19 14:32:22 -0500

    Update prAPI.py

--- RN/prAPI.py
+++ RN/prAPI.py
@@ -319,7 +319,7 @@ class prAPI(commands.Cog):
     async def fetch_nations_list(self, query: str):
         headers = {"User-Agent": await self.config.useragent()}
         url = f"https://www.nationstates.net/cgi-bin/api.cgi?region=the_wellspring&q={query}"
-        if qurey.lower() == "wanations":
+        if query.lower() == "wanations":
             query = "UNNATIONS"
     
         async with self.session.get(url, headers=headers) as response:
