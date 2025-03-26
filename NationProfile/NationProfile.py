63c760d2992b3fffd48ed756f05e9b0056308cdb
jmikk <buldog.buldog26@gmail.com>
2025-03-26 16:51:58 -0500

    Update NationProfile.py

--- NationProfile/NationProfile.py
+++ NationProfile/NationProfile.py
@@ -127,7 +127,7 @@ class NationProfile(commands.Cog):
 
         await ctx.send("What is your nation's population? (between 100,000 and 6,000,000)")
         while True:
-        try:
+            try:
                 population_input = await self.bot.wait_for('message', check=check, timeout=60)
             raw_pop = int(population_input.content.replace(",", ""))
             if 100000 <= raw_pop <= 6000000:
