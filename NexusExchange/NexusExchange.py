1c8656772919e992b7f7762807fc029c3b1e6e09
jmikk <buldog.buldog26@gmail.com>
2025-04-28 08:22:23 -0500

    Update NexusExchange.py

--- NexusExchange/NexusExchange.py
+++ NexusExchange/NexusExchange.py
@@ -1777,7 +1777,7 @@ Helpful Resources:
         
     @commands.guild_only()
     @commands.command()
-    async def balance(self, ctx, secret: bool = True, member: discord.Member = None, currency_name: str = None):
+    async def balance(self, ctx, secret: bool = False, member: discord.Member = None, currency_name: str = None):
         """
         Check your or another member's balance of WellCoins, XP, or a specific mini-currency.
         Usage: [p]balance [@member] [currency_name]
