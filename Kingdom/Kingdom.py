7332090738d5371e9e25202711d3dc1bd982a0ef
jmikk <buldog.buldog26@gmail.com>
2025-06-12 16:29:31 -0500

    Update Kingdom.py

--- Kingdom/Kingdom.py
+++ Kingdom/Kingdom.py
@@ -376,8 +376,8 @@ class Kingdom(commands.Cog):
     def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(None, identifier=345678654456, force_registration=True)
-        self.config.register_guild(projects=[], completed_projects=[],personal_projects=[],tax_credits=0)
-        self.config.register_user(completed_personal_projectz=[])
+        self.config.register_guild(projects=[], completed_projects=[],personal_projects=[])
+        self.config.register_user(completed_personal_projectz=[],tax_credits=0)
     
     async def get_incomplete_personal_projects(self, user, guild):
         all_projects = await self.get_personal_projects(guild)
