3062298c246a5efc4a71a395dbd767e52adaea57
jmikk <buldog.buldog26@gmail.com>
2025-06-12 21:25:33 -0500

    Update NexusExchange.py

--- NexusExchange/NexusExchange.py
+++ NexusExchange/NexusExchange.py
@@ -1696,84 +1696,6 @@ Helpful Resources:
                         else:
                             await ctx.send(execute_response.text)
                             await ctx.send("Failed to execute the gift.")
-
-    @commands.guild_only()
-    @commands.admin()
-    @commands.command()
-    async def add_to_currency(self, ctx, user: discord.Member, currency_name: str, amount: int):
-        """Add a certain amount of a mini-currency to a user's balance."""
-        currency_name = currency_name.lower().replace(" ", "_")
-        exchange_rates = await self.config.guild(ctx.guild).exchange_rates()
-
-        if currency_name not in exchange_rates:
-            await ctx.send("This currency does not exist.")
-            return
-
-        config_id = exchange_rates[currency_name]["config_id"]
-        mini_currency_config = Config.get_conf(None, identifier=config_id, force_registration=True)
-        
-        # Retrieve user's current balance
-        user_balance = await mini_currency_config.user(user).get_raw(currency_name, default=0)
-        new_balance = user_balance + amount
-
-        # Update balance
-        await mini_currency_config.user(user).set_raw(currency_name, value=new_balance)
-
-        await ctx.send(f"Added `{amount}` `{currency_name}` to {user.mention}. New balance: `{new_balance}`.")
-
-    @commands.guild_only()
-    @commands.admin()
-    @commands.command()
-    async def add_currency(self, ctx, currency_name: str, config_id: int, rate: float):
-        """Add a new mini-currency with its config ID and exchange rate."""
-        currency_name = currency_name.lower().replace(" ","_")
-        async with self.config.guild(ctx.guild).exchange_rates() as exchange_rates:
-            exchange_rates[currency_name] = {"config_id": config_id, "rate": rate}
-        await ctx.send(f"Added `{currency_name}` with exchange rate `{rate}` from config `{config_id}`.")
-
-    @commands.guild_only()
-    @commands.admin()
-    @commands.command()
-    async def remove_currency(self, ctx, currency_name: str):
-        """Remove a mini-currency from the exchange list."""
-        currency_name = currency_name.lower().replace(" ","_")
-        async with self.config.guild(ctx.guild).exchange_rates() as exchange_rates:
-            if currency_name not in exchange_rates:
-                await ctx.send("This currency does not exist.")
-                return
-            del exchange_rates[currency_name]
-        await ctx.send(f"Removed `{currency_name}` from the exchange list.")
-
-    @commands.guild_only()
-    @commands.command()
-    async def exchange(self, ctx, currency_name: str, amount: int):
-        """Convert a mini-currency into WellCoins."""
-        currency_name = currency_name.lower().replace(" ","_")
-        exchange_rates = await self.config.guild(ctx.guild).exchange_rates()
-        
-        if currency_name not in exchange_rates:
-            await ctx.send("This currency is not available for exchange.")
-            return
-
-        config_id = exchange_rates[currency_name]["config_id"]
-        rate = exchange_rates[currency_name]["rate"]
-        mini_currency_config = Config.get_conf(None, identifier=config_id)
-        user_balance = await mini_currency_config.user(ctx.author).get_raw(currency_name, default=0)
-
-        if user_balance < amount:
-            await ctx.send("You do not have enough of this currency.")
-            return
-
-        new_wellspring_coins = int(amount * rate)
-        
-        # Deduct from mini-currency
-        await mini_currency_config.user(ctx.author).set_raw(currency_name, value=user_balance - amount)
-        
-        # Add to master currency
-        master_balance = await self.config.user(ctx.author).master_balance()
-        await self.config.user(ctx.author).master_balance.set(master_balance + new_wellspring_coins)
-        
-        await ctx.send(f"Exchanged `{amount}` `{currency_name}` for `{new_wellspring_coins}` WellCoins!")
         
     @commands.guild_only()
     @commands.command()
@@ -1816,38 +1738,6 @@ Helpful Resources:
         
             result_msg = f"💱 {member.display_name} has `{user_balance:.2f}` `{currency_name}`."
             await ctx.author.send(result_msg) if secret else await ctx.send(result_msg)
-
-
-
-
-    @commands.guild_only()
-    @commands.command()
-    async def rates(self, ctx):
-        """View current exchange rates."""
-        exchange_rates = await self.config.guild(ctx.guild).exchange_rates()
-        if not exchange_rates:
-            await ctx.send("No currencies have been added yet.")
-            return
-        
-        embed = discord.Embed(title="Exchange Rates", color=discord.Color.blue())
-        for currency, data in exchange_rates.items():
-            embed.add_field(name=currency, value=f"Rate: 1 {currency} will get you `{data['rate']}` Wellcoins (Config: `{data['config_id']}`)", inline=False)
-        
-        embed.add_field(name="How to get the various currencies", value="**Gold** Comes from Hunger Games! Join us every Saturday\n\n**Credits** come from recruiting, if you are instread in joining our recruiting force please reach out to 9006! ", inline=False)
-        await ctx.send(embed=embed)
-
-    @commands.guild_only()
-    @commands.admin()
-    @commands.command()
-    async def set_rate(self, ctx, currency_name: str, rate: float):
-        """Change the exchange rate for a mini-currency."""
-        currency_name = currency_name.lower().replace(" ","_")
-        async with self.config.guild(ctx.guild).exchange_rates() as exchange_rates:
-            if currency_name not in exchange_rates:
-                await ctx.send("That currency does not exist.")
-                return
-            exchange_rates[currency_name]["rate"] = rate
-        await ctx.send(f"Updated `{currency_name}` exchange rate to `{rate}`.")
 
     @commands.guild_only()
     @commands.command()
