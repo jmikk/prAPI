a468fe6d364ef255a43fa3ebab38c49d2d13b80b
jmikk <buldog.buldog26@gmail.com>
2025-07-18 21:37:45 -0500

    Update Casino.py

--- Casino/Casino.py
+++ Casino/Casino.py
@@ -212,8 +212,8 @@ class Casino(commands.Cog):
         await self.config.user(user).history.set(user_history)
 
 
-    #@commands.command()
-    #@cooldown(1, 3, BucketType.guild)
+    @commands.command()
+    @cooldown(1, 3, BucketType.guild)
     @commands.admin()
     async def roulette(self, ctx, bet: float, call: str):
         """Play roulette. Bet on a number (0-36), red, black, even, or odd."""
