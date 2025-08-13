ce573275e7ff634d70b3c11aaea07f2cf6d11b72
jmikk <buldog.buldog26@gmail.com>
2025-08-13 16:41:54 -0500

    Update Farm.py

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -360,7 +360,10 @@ class Farm(commands.Cog):
 
         user_rep = user_data['rep']
         total_stats = sum([user_data['strength'],user_data['defense'],user_data['speed'],user_data['luck'],user_data['Health'],user_data['rep']]) / 6
-        low_mod = max(1, total_stats - 10)
+        
+        await ctx.send(f"DEBUG: avg: {total_stats}")
+        
+        low_mod = max(1, total_stats - 100)
         high_mod = max(5, total_stats + user_rep)
         enemy_stats = {
             "strength": random.randint(math.floor(1 + user_rep / low_mod), math.ceil((user_rep + 1) * high_mod)),
