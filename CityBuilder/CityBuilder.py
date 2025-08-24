910e8c216cc4d34e831a5279d498410a7164f50a
jmikk <buldog.buldog26@gmail.com>
2025-08-24 16:22:06 -0500

    Update CityBuilder.py

--- CityBuilder/CityBuilder.py
+++ CityBuilder/CityBuilder.py
@@ -870,7 +870,7 @@ class CityBuilder(commands.Cog):
         return int((time.time() // TICK_SECONDS + 1) * TICK_SECONDS)
 
     def _all_tiers(self) -> list:
-    """Sorted unique tiers from BUILDINGS."""
+        """Sorted unique tiers from BUILDINGS."""
         tiers = sorted({int(data.get("tier", 0)) for data in BUILDINGS.values()})
         return tiers
 
