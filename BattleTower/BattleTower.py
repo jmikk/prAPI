99dc28cf5ab79cf64113355165157a88b5a698ee
jmikk <buldog.buldog26@gmail.com>
2025-10-30 22:13:00 -0500

    e

--- BattleTower/BattleTower.py
+++ BattleTower/BattleTower.py
@@ -165,7 +165,7 @@ class BattleTowerView(discord.ui.View):
 
         self.autosim_running = False
 
-        self.autosim_delay = 10.0  # seconds between autosim actions
+        self.autosim_delay = 2.0  # seconds between autosim actions
 
 
 
