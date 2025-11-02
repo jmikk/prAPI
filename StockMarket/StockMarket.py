e2625a7e8143d6c04ac995eb451bbbd74b4dffbe
jmikk <buldog.buldog26@gmail.com>
2025-11-02 10:51:37 -0600

    Update StockMarket.py

--- StockMarket/StockMarket.py
+++ StockMarket/StockMarket.py
@@ -446,8 +446,12 @@ class StockMarket(commands.Cog):
                 else:
                     base_percent = random.uniform(-2, 2)
     
+                # Market activity influence
+                market_change = 0.01 * (self.last_day_trades / 100000)
+                market_change = max(-0.05, min(market_change, 0.04)) 
+    
                 # Final percent change calculation
-                total_percent_change = base_percent + tag_bonus 
+                total_percent_change = base_percent + tag_bonus + (market_change * 100)
                 new_price = round(old_price * (1 + total_percent_change / 100), 2)
     
                 # Bankruptcy/delist logic
