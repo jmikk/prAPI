b6a138acd72283e5161112bfb8e35227129fe00e
jmikk <buldog.buldog26@gmail.com>
2025-12-09 14:35:17 -0600

    Update WAO.py

--- WAO/WAO.py
+++ WAO/WAO.py
@@ -796,7 +796,7 @@ class WAO(commands.Cog):
 
         # Reserve second post for IFV
         try:
-            command_example = f"[p]waobserver ifv {thread.id}"
+            command_example = f"waobserver ifv {thread.id}"
             ifv_placeholder = await thread.send(
                 f"*This post is reserved for the IFV.*\n\n"
                 f"Use this command to set it:\n`{command_example}`"
