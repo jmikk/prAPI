3d11fd0d4b9fb5a49b406bc06aa6126e5742c2fe
jmikk <buldog.buldog26@gmail.com>
2026-01-07 18:39:59 -0600

    Update WAO.py

--- WAO/WAO.py
+++ WAO/WAO.py
@@ -1002,6 +1002,8 @@ class WAO(commands.Cog):
 
         approvals_list = [a for a in approvals.split(":") if a]
         approvals_count = len(approvals_list)
+        if approvals_count < 20:
+            return
 
         if proposed_by != "Unknown":
             pb_slug = self._ns_slug(proposed_by)
