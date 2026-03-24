82e729d477dd634f48e78bc617e93b5db018b706
jmikk <buldog.buldog26@gmail.com>
2026-03-24 17:29:35 -0500

    Update NexusCards.py

--- NexusCards/NexusCards.py
+++ NexusCards/NexusCards.py
@@ -140,6 +140,7 @@ class NexusCards(commands.Cog):
             return await ctx.send("No eligible cards found in 9006.")
 
         target = random.choice(eligible)
+        await ctx.send(target)
         card_id = target.find("ID").text
         season = target.find("SEASON").text
         name = target.find("NAME").text if target.find("NAME") is not None else "Unknown Name"
