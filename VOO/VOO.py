652a7084b7b1229e4999b9c76d4e8da8d355b88d
jmikk <buldog.buldog26@gmail.com>
2026-07-26 11:55:18 -0500

    Update VOO.py

--- VOO/VOO.py
+++ VOO/VOO.py
@@ -779,7 +779,7 @@ class VOO(commands.Cog):
     @voo_group.command(name="clearqueue")
     async def clear_queue(self, ctx: commands.Context):
         """Clear the entire global shared queue."""
-        await self.config.shared_queue.set([])
+        await self.config.shared_queue.set(queue[:100])        
         await self._refresh_all_embeds()
         await ctx.send("Global shared queue cleared.")
 
