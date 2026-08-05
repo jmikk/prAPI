c18088aeffc9811b38c3a74054fcb24e97631448
jmikk <buldog.buldog26@gmail.com>
2026-08-04 19:24:15 -0500

    Update NexusCards.py

--- NexusCards/NexusCards.py
+++ NexusCards/NexusCards.py
@@ -243,6 +243,7 @@ class NexusCards(commands.Cog):
 
         url = f"https://www.nationstates.net/cgi-bin/api.cgi?q=card+info+owners;cardid={card_id};season={season}"
         root, _ = await self._ns_request(url, ctx=ctx)
+        await ctx.send(root)
         owners = [o.text.lower() for o in root.findall(".//OWNER")]
         if "9005" in owners:        
             found_in = "9005"
@@ -256,7 +257,6 @@ class NexusCards(commands.Cog):
                 locked_cards = await giveaway_cog.get_all_locked_cards()
                 
                 card_key = f"{card_id}_{season}"
-                await ctx.send(card_data)
                 if card_key in locked_cards:
                     # Stop or skip the card because it's locked by the giveaway system!
                     return await ctx.send("⚠️ This card is currently claimed or tied up in an active giveaway.")
