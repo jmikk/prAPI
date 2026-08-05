e609d3ee70cb0ca6550b8f200bf391d6f4d88123
jmikk <buldog.buldog26@gmail.com>
2026-08-04 19:31:05 -0500

    Update NexusCards.py

--- NexusCards/NexusCards.py
+++ NexusCards/NexusCards.py
@@ -243,25 +243,12 @@ class NexusCards(commands.Cog):
 
         url = f"https://www.nationstates.net/cgi-bin/api.cgi?q=card+info+owners;cardid={card_id};season={season}"
         root, _ = await self._ns_request(url, ctx=ctx)
-
         owners = [o.text.lower() for o in root.findall(".//OWNER")]
         if "9005" in owners:        
             found_in = "9005"
             card_data = root
-        
         elif "the_phoenix_of_the_spring" in owners:
-            # Inside another cog or command:
-            giveaway_cog = self.bot.get_cog("GiveawayCog")
-            
-            if giveaway_cog:
-                locked_cards = await giveaway_cog.get_all_locked_cards()
-                for item in locked_cards:
-                    await ctx.send(item)
-                card_key = f"{card_id}_{season}"
-                if card_key in locked_cards:
-                    # Stop or skip the card because it's locked by the giveaway system!
-                    return await ctx.send("⚠️ This card is currently claimed or tied up in an active giveaway.")
-            
+            return await ctx.send("This card is on the_phoenix_of_the_spring but the good news is 9005 can move it for you as long as it is not part of a giveaway.")
         else: 
             return await ctx.send("Legendary not found in stockpiles. Check out 9005 and The Phoenix of the Spring's deck for the cards you can claim")
 
